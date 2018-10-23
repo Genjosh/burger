05a35b916010d5be855df0e73a72e116a8b85645
Genjosh <joshromerogomez@gmail.com>
2018-10-22 19:26:56 -0600

    final-commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgers_name, devoured) VALUES ("Bacon Burger",False);
+INSERT INTO burgers (burgers_name, devoured) VALUES ("Cheese Burger", False);
+INSERT INTO burgers (burgers_name, devoured) VALUES ("Smash Burger", False);
---
+++ models/burger.js
@@ -0,0 +1,27 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devoured"
+    ], [
+      name, false
+    ], cb);
+  },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devoured: true
+    }, condition, cb);
+  },
+
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+};
+
+
+module.exports = burger;
+
